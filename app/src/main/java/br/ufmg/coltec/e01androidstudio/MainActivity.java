39a8ba3eaafeacb386d5887e1823dab1e85d5294
mairamml <mairaaml11@gmail.com>
2024-08-13 12:14:13 -0300

    Formatação do código e Monitoramento do app feitos.

--- app/src/main/java/br/ufmg/coltec/e01androidstudio/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/e01androidstudio/MainActivity.java
@@ -3,6 +3,7 @@ package br.ufmg.coltec.e01androidstudio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -10,17 +11,32 @@ import android.widget.Toast;
 
 // TODO Reorganizar esse código
 public class MainActivity extends AppCompatActivity {
-@Override protected void onCreate(Bundle savedInstanceState) {
-super.onCreate(savedInstanceState); setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-Button btnOk = this.findViewById(R.id.btn_ok); Button btnErro = this.findViewById(R.id.btn_erro);
-btnOk.setOnClickListener(new View.OnClickListener() {@Override public void onClick(View view) { Toast t = Toast.makeText(MainActivity.this, "Aprovado!", Toast.LENGTH_SHORT); t.setGravity(Gravity.TOP, 0, 0); t.show(); } });
-btnErro.setOnClickListener(new View.OnClickListener() {
-@Override
-public void onClick(View view) { Toast t = Toast.makeText(MainActivity.this, "Reprovado!", Toast.LENGTH_LONG); t.setGravity(Gravity.TOP, 0, 0); t.show();
-// TODO Acrescentar um log de warning, indicando que o usuário clicou neste botão
-}
-});
-// TODO Acrescentar um log verbose, indicando que a Activity foi criada corretamente
-}
+        Button btnOk = this.findViewById(R.id.btn_ok);
+        Button btnErro = this.findViewById(R.id.btn_erro);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast t = Toast.makeText(MainActivity.this, "Aprovado!", Toast.LENGTH_SHORT);
+                t.setGravity(Gravity.TOP, 0, 0);
+                t.show();
+            }
+        });
+        btnErro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast t = Toast.makeText(MainActivity.this, "Reprovado!", Toast.LENGTH_LONG);
+                t.setGravity(Gravity.TOP, 0, 0);
+                t.show();
+                // TODO Acrescentar um log de warning, indicando que o usuário clicou neste botão
+                Log.w("MainActivity.java", "Botão de erro foi clicado!");
+            }
+        });
+        // TODO Acrescentar um log verbose, indicando que a Activity foi criada corretamente
+        Log.v("MainActivity.java", "Activity foi criada corretamente!");
+    }
 }
